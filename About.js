5342625d67c715ccaaccf7bd25d60c2b09d366e6
ProgrammingAgainstBrutality <maskwell@criptext.com>
2020-05-31 21:41:39 -0400

    create basic About page

---
+++ About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h2>How to Contribute</h2>
+      <p>The content on this site is static - there is no way to directly submit content. If you have a video that you would like to contribute, please provide the information by <a href="https://github.com/ProgrammingAgainstBrutality/end-police-violence/issues/new">opening a ticket here</a>. Please provide context and source link, prefferably from Reddit, Twitter, Youtube or Streamable so the content and be downloaded and preserved.</p>
+      <p>You can also contribute by downloading copies of these videos to help ensure they are preserved and continue to be shared in the event that the current videos and sources are removed.</p>
+      <p>If you have some experience working with GitHub, please feel free to fork, clone and host your own copy of this website.</p>
+    </div>
+    <div>
+      <h2>About this Site</h2>
+      <p>The creators of this site do not condone or encourage violence.</p>
+      <p>This site is dedicated to sharing and preserving records of police violence during the 2020 protests sparked by the murder of George Floyd by ex-cop Derek Chauvin.</p>
+      <p>From Martin Luther King Jr.:</p>
+      <quote>I think America must see that riots do not develop out of thin air. Certain conditions continue to exist in our society which must be condemned as vigorously as we condemn riots. But in the final analysis, a riot is the language of the unheard. And what is it that America has failed to hear? It has failed to hear that the plight of the Negro poor has worsened over the last few years. It has failed to hear that the promises of freedom and justice have not been met. And it has failed to hear that large segments of white society are more concerned about tranquility and the status quo than about justice, equality, and humanity. And so in a real sense our nation’s summers of riots are caused by our nation’s winters of delay. And as long as America postpones justice, we stand in the position of having these recurrences of violence and riots over and over again. Social justice and progress are the absolute guarantors of riot prevention.</quote>
+    </div>
+  )
+}
