757c497a2db7a74ad90bf160a15676250c954bef
maxwellbenton <maxwellbenton@gmail.com>
2020-06-03 12:52:20 -0400

    convert to class for easier sorting

--- src/App.js
+++ src/App.js
@@ -1,61 +1,72 @@
 import React, {useState, useEffect} from 'react';
-import VideoCard from './VideoCard'
+
 import About from './About'
 import HowToDownload from './HowToDownload'
+import VideoRows from './VideoRows'
 import './App.css';
 
-function App() {
-  const [videos, setVideos] = useState([])
+class App extends React.Component {
 
-  const getVideos = () => {
+  state = {
+    videos: [],
+    sortBy: ''
+  }
+
+  componentDidMount() {
     fetch('https://raw.githubusercontent.com/ProgrammingAgainstBrutality/end-police-violence/master/videos.json')
     .then(response => response.json())
-    .then(videos => setVideos(videos))
+    .then(videos => this.setState({videos}))
   }
 
-  useEffect(() => {
-    getVideos()
-    console.log('only on start')
-  }, [])
+  onSort = (event, sortKey) => {
+    const videos = this.state.videos;
+    let sortBy = this.state.sortBy;
+    videos.sort((a,b) => a[sortKey].localeCompare(b[sortKey]))
 
-  useEffect(() => {
-    console.log(videos)
-  }, [videos])
-
-  const onSort = (event, sortKey) => {
-    const data = videos;
-    data.sort((a,b) => a[sortKey].localeCompare(b[sortKey]))
-    setVideos(data)
+    if (sortBy === sortKey) {
+      videos.reverse();
+      sortBy = sortKey + 'Reverse'
+    } else {
+      sortBy = sortKey
+    }
+    
+    this.setState((prevState) => ({
+      videos,
+      sortBy
+    }))
   }
 
-  return (
-    <div className="App">
-      <header>
-       <h1 className="title">End Police Violence</h1>
-       <h2 className="subtitle">Records of Police Brutality During the 2020 George Floyd Protests</h2>
-       <h4 className="warning"><strong>Please be advised - Videos contain graphic content</strong></h4>
-       <About />
-       <HowToDownload />
-      </header>
-      <main>
-      <table>
-        <thead>
-          <tr>
-            <th>Video</th>
-            <th onClick={e => onSort(e, 'title')}>Description</th>
-            <th onClick={e => onSort(e, 'location')}>Location</th>
-            <th onClick={e => onSort(e, 'source')}>Original Source</th>
-            <th onClick={e => onSort(e, 'recordingDate')}>Recording Date</th>
-            <th onClick={e => onSort(e, 'uploadDate')}>Upload Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {videos.map(video => <VideoCard key={video.id} {...video}/>)}
-        </tbody>
-      </table>
-      </main>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header>
+         <h1 className="title">End Police Violence</h1>
+         <h2 className="subtitle">Records of Police Brutality and Excessive Force During the 2020 George Floyd Protests</h2>
+         <h4 className="warning"><strong>Videos contain graphic content</strong></h4>
+         <About />
+         <HowToDownload />
+        </header>
+        <main>
+        <table>
+          <thead>
+            <tr>
+              <th>Video</th>
+              <th onClick={e => this.onSort(e, 'title')}>Description</th>
+              <th onClick={e => this.onSort(e, 'location')}>Location</th>
+              <th onClick={e => this.onSort(e, 'source')}>Original Source</th>
+              <th onClick={e => this.onSort(e, 'recordingDate')}>Recording Date</th>
+              <th onClick={e => this.onSort(e, 'uploadDate')}>Upload Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <VideoRows videos={this.state.videos}/>
+          </tbody>
+        </table>
+        </main>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
